fa4f152a343be831105eee39008dea265c9ecbf8
soulongandgoodnight <dev.nuggets@yandex.com>
2026-02-12 14:08:56 +0200

    minor visual improvements

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto create(ItemDto itemDto, Long userId);
+
     ItemDto update(Long itemId, ItemDto itemDto, Long userId);
+
     ItemDto getById(Long itemId);
+
     List<ItemDto> getAllByOwner(Long userId);
+
     List<ItemDto> search(String text);
 }
